e147a8000cd5fe10dfa761a4bbbfc617d905d03a
Alex Olsson <alex@dumbfucks.se>
2024-04-02 15:22:01 +0200

    Update RemoveUser.ps1
    Updated the script with gpo redirect compatibility

--- RemoveUser.ps1
+++ RemoveUser.ps1
@@ -5,34 +5,56 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     exit
 }
 
+function TakeOwnership {
+    Param(
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]$Path
+    )
+    takeown /f $Path /r /d y
+}
+
+function GrantFullControl {
+    Param(
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]$Path,
+        [Parameter(Mandatory=$true, Position=1)]
+        [string]$User
+    )
+    $acl = Get-Acl $Path
+    $permission = "localmachine\$User","FullControl","Allow" #Add local admin as acl entry
+    $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
+    $acl.SetAccessRule($accessRule)
+    Set-Acl $Path $acl
+}
+
 function RemoveUser {
-    Param([Parameter(Mandatory=$true, Position=0)][string]$User) 
+    Param(
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]$User
+    )
     Write-Host $User
-    
     Get-CimInstance -Class Win32_UserProfile | Where-Object { $_.LocalPath.split('\')[-1] -eq $User } | Remove-CimInstance
-    
-    #Uncomment the Line Below if you have a folder with the same name as the username you are removing that you also want to delete
-    #Remove-Item "U:\users\$($User)" -Force -Recurse
+    Remove-Item "G:\users\$($User)\" -Force -Recurse
 }
 
 Add-Type -AssemblyName Microsoft.VisualBasic
+
 $UserName = [Microsoft.VisualBasic.Interaction]::InputBox('Enter the username of the User you wish to remove', 'UserName', "$env:UserName")
+$UsrNm = 'xx.xxxxx.xx\' + $UserName # Enter domain
+Write-Host $UsrNm
 
-Write-Host $UserName
+$confirmResult = [System.Windows.Forms.MessageBox]::Show("Are you sure you want to remove the user $UsrNm ?", "Confirm removal", "YesNo", "Warning")
 
-$yeah = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes","I've checked that the information is correct and that I'm sure I want to remove this user."
-$nah = New-Object System.Management.Automation.Host.ChoiceDescription "&No","I've missed something."
-$options = [System.Management.Automation.Host.ChoiceDescription[]]($yeah, $nah)
-$heading = "Are you sure?"
-$mess = "Are you sure you want to remove the user $UserName ?"
-$rslt = $host.ui.PromptForChoice($heading, $mess, $options, 1)
-switch ($rslt) {
-0{
-Write-Host "Yes" -ForegroundColor Green
-RemoveUser $UserName
-}1{
-Write-Host "No" -ForegroundColor Red
-PAUSE
+switch ($confirmResult) {
+    "Yes" {
+        Write-Host "Yes" -ForegroundColor Green
+        TakeOwnership "G:\users\$UserName\Downloads"
+        GrantFullControl "G:\users\$UserName\Downloads" "Administrator"
+        RemoveUser $UserName
+        Write-Host "$UsrNm"
+    }
+    "No" {
+        Write-Host "No" -ForegroundColor Red
+        PAUSE
+    }
 }
-}
-
