a0165b6f488d736e874a9f9d52f45c99d0116604
Alex Olsson <alex@dumbfucks.se>
2024-09-05 09:39:33 +0200

    Update ChangePrinterDriver.ps1

--- ChangePrinterDriver.ps1
+++ ChangePrinterDriver.ps1
@@ -1,30 +1,27 @@
-# Specific variables
-$printerPartialName = "xxxx" # Name of the printer. Remember to include the whole name apart from sessionnumber in Citrix session cases
-$newDriverName = "xxxx" # Exact name of the driver. Make sure it's installed on the server beforehand
 
-# Find printers matching the partial name supplied.
-$printers = Get-Printer | Where-Object { $_.Name -like "$printerPartialName*" }
+$printerPartialName = "xx\[UPD:PDF\] \(from x\) in session "
+$newDriver = "xx"
 
-if ($printers.Count -eq 0) {
-    Write-Host "No printer matching the name $printerPartialName was found."
-    exit
+# Get all printers that match the name
+$printers = Get-WmiObject -Query "SELECT * FROM Win32_Printer" | Where-Object { $_.Name -match "$printerPartialName\d+" }
+
+if ($printers) {
+    foreach ($printer in $printers) {
+        $printerName = $printer.Name  # Get the complete name of the printer as a string
+        Write-Output "Found printer: $printerName"
+        
+        try {
+            # Change Driver
+            $printer.DriverName = $newDriver
+            $printer.Put()
+
+            Write-Output "The driver has been changed to $newDriver for the printer $printerName"
+        } catch {
+            Write-Output "Failed to change driver"
+        }
+    }
+} else {
+    Write-Output "No printers found matching $printerPartialName"
 }
 
-foreach ($printer in $printers) {
-    # Check if the new driver is installed
-    $driver = Get-PrinterDriver -Name $newDriverName -ErrorAction SilentlyContinue
-    if ($null -eq $driver) {
-        Write-Host "The driver $newDriverName could not be found."
-        exit
-    }
-
-    # Change driver
-    try {
-        Set-Printer -Name $printer.Name -DriverName $newDriverName
-        Write-Host "The driver has been changed too $newDriverName for the printer $printer.Name."
-    } catch {
-        Write-Host "Failed to change driver for $printer.Name: $_"
-    }
-}
-
-Write-Host "Finished."
+Write-Output "Finished."
